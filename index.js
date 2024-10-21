e579c8b8ae0fc0cfed90c9f5ffb2249e447978e0
= <=>
2024-10-21 12:29:22 -0300

    Definindo a Página Inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const host  = "0.0.0.0";
+const porta = 3000;
+const app = express(); // todas as interfaces de rede do computador 
+
+function paginaIncial(requisicao, resposta){
+    resposta.send(`<h1> Seja Bem Vindo digite a tabuada que deseja procurar </h1>
+                    <h2>http://localhost/?tabuada=3&sequencia=25;</h2>
+                    <br/>
+
+        `);
+}
+
+
+app.get("/", paginaIncial);
+
+app.listen(porta, host, () =>{
+    console.log("Servidor em execução http://" + host + ":" + porta);
+});
