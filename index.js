ddb61dabc7728fb3aff16b12a9e605e4cd7ca466
= <=>
2024-10-21 13:21:02 -0300

    mudança no codigo

--- index.js
+++ index.js
@@ -4,18 +4,10 @@ const host = "0.0.0.0";
 const porta = 3000;
 const app = express(); 
 
-// Rota principal com instruções
-app.get("/", paginaInicial);
 
-function paginaInicial(requisicao, resposta) {
-    resposta.send(`
-        <h1>Seja Bem-Vindo! Digite a tabuada que deseja procurar</h1>
-        <h2>Exemplo: <a href="/Tabuada?tabuada=3&sequencia=25">http://localhost:3000/Tabuada?tabuada=3&sequencia=25</a></h2>
-    `);
-}
 
 // Rota para calcular e exibir a tabuada
-app.get("/Tabuada", tabuada);
+app.get("/", tabuada);
 
 function tabuada(requisicao, resposta) {
     const tabuada = parseInt(requisicao.query.tabuada);
