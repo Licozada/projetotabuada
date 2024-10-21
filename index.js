b702df16db8e16412fa48d57987b9b97f35e5f63
= <=>
2024-10-21 13:55:35 -0300

    mudando a logica do codigo, pois estava validando tudo e é para validar apenas se a sequencia foi informada

--- index.js
+++ index.js
@@ -2,27 +2,18 @@ import express from "express";
 
 const host = "0.0.0.0";
 const porta = 3000;
-const app = express(); 
+const app = express();
 
-
-
-// Rota para calcular e exibir a tabuada
 app.get("/", tabuada);
 
 function tabuada(requisicao, resposta) {
     const tabuada = parseInt(requisicao.query.tabuada);
-    const sequencia = parseInt(requisicao.query.sequencia);
-
-    if (isNaN(tabuada) || isNaN(sequencia) || sequencia < 0) {
-        resposta.write('<h1>Erro: Insira valores numéricos válidos para "tabuada" e "sequencia".</h1>');
-        resposta.end();
-        return;
-    }
+    const sequencia = parseInt(requisicao.query.sequencia) || 10; // Define 10 se não for passado nenhum valor
 
     resposta.write(`<h1>Tabuada do ${tabuada}</h1>`);
     resposta.write("<ul>");
 
-    // O laço agora vai de 0 até o valor informado em `sequencia`
+    // Gera a tabuada até o valor de `sequencia`
     for (let i = 0; i <= sequencia; i++) {
         resposta.write(`<li>${tabuada} x ${i} = ${tabuada * i}</li>`);
     }
