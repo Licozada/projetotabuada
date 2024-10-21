6b9c192246f134e58e57ffca34713e432b23ab09
= <=>
2024-10-21 12:49:17 -0300

    codigo ta tabuada feito

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express(); // todas as interfaces de rede do computador
 
 function paginaIncial(requisicao, resposta){
     resposta.send(`<h1> Seja Bem Vindo digite a tabuada que deseja procurar </h1>
-                    <h2>http://localhost/?tabuada=3&sequencia=25;</h2>
+                    <h2> Exemplo http://localhost/?tabuada=3&sequencia=25;</h2>
                     <br/>
 
         `);
